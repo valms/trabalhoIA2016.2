67b1a6cc22920fe36153adec3cf91025e6e375de
Francisco Valmar Isaias Silva Júnior <valmar.junior1@gmail.com>
2016-09-08 15:53:03 -0300

    [002] Início da implementação do Trabalho. Testes com os agentes e seus respectivos sensores. Definição dos nomes das variáveis (em inglês) das pecepções do agente-ladrão.
    Signed-off-by: Francisco Valmar Isaias Silva Júnior <valmar.junior1@gmail.com>

--- src/algoritmo/Ladrao.java
+++ src/algoritmo/Ladrao.java
@@ -1,10 +1,60 @@
 package algoritmo;
 
+/**
+ * UNIVERSIDADE DE FORTALEZA - I.A. 2016.2
+ * 
+ * @author Valmar Júnior - 1120793 / 6
+ * @author João Pedro
+ *
+ */
+@SuppressWarnings(value = { "all" })
 public class Ladrao extends ProgramaLadrao {
 
-    public int acao() {
+	/**
+	 * VARIÁVEIS QUE CONTROLAM A AÇÃO DO AGENTE-LADRÃO (VER PÁGINA 4 DO MANUAL
+	 * DO JOGO)
+	 */
+	private final int MOVEMENT_PARADO 	= 0;
+	private final int MOVEMENT_UP 		= 1;
+	private final int MOVEMENT_DOWN 	= 2;
+	private final int MOVEMENT_RIGHT 	= 3;
+	private final int MOVEMENT_LEFT 	= 4;
 
-        return (int) (Math.random() * 5);
-    }
+	/**
+	 * VARIÁVEIS REPRESENTANDO OS VALORES DE OFALTO DO AGENTE-LADRÃO (VIDE
+	 * TABELA 01 DO MANUAL)
+	 */
+	private final int VISION_BLIND 		= -2;
+	private final int VISION_OUTSIDE	= -1;
+	private final int VISION_EMPTY 		= 0;
+	private final int VISION_WALL 		= -2;
+	private final int VISION_BANK 		= -2;
+	private final int VISION_COIN 		= -2;
+	private final int VISION_POWER_UP 	= -2;
+	private final int VISION_SAVER 		= -2;
+	private final int VISION_THIEF 		= -2;
+	
+	/**
+	 * VARIÁVEIS REPRESENTANDO OS VALORES DE FERMÔNIO DO AGENTE-LADRÃO (VIDE
+	 * TABELA 02 DO MANUAL)
+	 */
+
+	private final int PHEREOMONE_EMPTY 				= 0;
+	private final int PHEREOMONE_ONE_STEP_AGO 		= 1;
+	private final int PHEREOMONE_TWO_STEPS_AGO 		= 2;
+	private final int PHEREOMONE_THREE_STEPS_AGO 	= 3;
+	private final int PHEREOMONE_FOUR_STEPS_AGO 	= 4;
+	private final int PHEREOMONE_FIVE_STEPS_AGO 	= 5;
+	
+
+	public int acao() {
+
+		System.out.println(sensor.getPosicao());
+		// for (int i = 0; i < sensor.getVisaoIdentificacao().length; i++) {
+		// System.out.println();
+		// System.out.println(sensor.getVisaoIdentificacao()[i]);
+		// }
+		return (int) (Math.random() * 5);
+	}
 
 }
